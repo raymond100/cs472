1aeb36d184cfa37b495799ec2500e0f207825d80
ray100 <jraymonddoirin@gmail.com>
2021-08-31 18:21:50 -0500

    add about template

---
+++ index.js
@@ -0,0 +1,15 @@
+fetch('./includes/_header.txt')
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+fetch('./includes/_footer.txt')
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
