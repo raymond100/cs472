29b27c978541f6cde3ead11d5ce6e52825a59152
ray100 <jraymonddoirin@gmail.com>
2021-09-03 15:03:16 -0500

    add tip calculator && fixed buggy js

---
+++ buggy/js/buggy3.js
@@ -0,0 +1,10 @@
+function gogogo() {
+    alert("Yay, it works!");
+}
+/*
+Error:
+- There was a semicolon after the declaration of the function.
+- There was a parentheses in place of a bracket.
+- The built in function is alert().
+- For the string eiter you use "" or '' not "'.
+**/
---
+++ buggy/js/buggy4.js
@@ -0,0 +1,12 @@
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+        if (5 < 2) {
+            alert("Smaller");
+        }
+    }
+}//Error: missing a bracket
+
+function gogogo() {
+    alert("Yay, it works!");
+}
