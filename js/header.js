7ef4738cdce4d68514935374cabfeb42b93eee87
ray100 <jraymonddoirin@gmail.com>
2021-09-06 00:04:01 -0500

    fixed tmp

--- js/header.js
+++ js/header.js
@@ -1,14 +1,11 @@
 (() => {
     const pathname = window.location.pathname;
     const host = window.location.host;
-    let relLocation;
+    let relLocation = '.';
     if (host === "127.0.0.1:5500") {
-        relLocation = './pages';
         if (pathname != "/index.html") {
-            relLocation = '../pages';
+            relLocation = '..';
         }
-    } else {
-        relLocation = '.';
     }
 
     document.querySelector('header').innerHTML = `
@@ -17,22 +14,22 @@
     <nav>
         <ul>
             <li><a href="${relLocation}/index.html">Home</a></li>
-            <li><a href="${relLocation}/about.html">About</a></li>
+            <li><a href="${relLocation}/pages/about.html">About</a></li>
             <div class="dropdown">
                 <a class="dropbtn">Homework
                     <i class="fa fa-angle-down"></i>
                 </a>
                 <div class="dropdown-content">
-                    <li><a href="${relLocation}/pie.html">Recipe</a></li>
-                    <li><a href="${relLocation}/tmnt.html">Movie</a></li>
-                    <li><a href="${relLocation}/grade.html">Grade</a></li>
-                    <li><a href="${relLocation}/signup.html">Sign up</a></li>
+                    <li><a href="${relLocation}/pages/pie.html">Recipe</a></li>
+                    <li><a href="${relLocation}/pages/tmnt.html">Movie</a></li>
+                    <li><a href="${relLocation}/pages/grade.html">Grade</a></li>
+                    <li><a href="${relLocation}/pages/signup.html">Sign up</a></li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
-                    <li><a href="${relLocation}/tip.html">Tip Calculator</a></li>
+                    <li><a href="${relLocation}/pages/tip.html">Tip Calculator</a></li>
                     <li><a href="${relLocation}/buggy/buggyjs.html">Buggy Js</a></li>
-                    <li><a href="${relLocation}/decoratemytext.html">Decorate Text</a></li>
+                    <li><a href="${relLocation}/pages/decoratemytext.html">Decorate Text</a></li>
                     <li><a href="${relLocation}/data/JavascriptExercises.pdf">JS Exercices</a></li>
                 </div>
             </div>
