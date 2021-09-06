744f5e98969c116c20ace9f46db482e3931ae5de
ray100 <jraymonddoirin@gmail.com>
2021-09-05 20:40:53 -0500

    try again

--- js/header.js
+++ js/header.js
@@ -4,39 +4,39 @@
     let relLocation;
     if (host === "127.0.0.1:5500") {
         if (pathname === "/index.html") {
-            relLocation = '.';
+            relLocation = './pages';
         } else {
-            relLocation = '..';
+            relLocation = '../pages';
         }
     } else {
         if (pathname === "/index.html") {
-            relLocation = 'cs472/.';
+            relLocation = 'cs472/pages';
         } else {
             relLocation = 'cs472/..';
         }
     }
     document.querySelector('header').innerHTML = `
     <div class="cover"></div>
-    <img src="${relLocation}/images/profile.png" alt="profile image" id="profile">
+    <img src="${relLocation}/../images/profile.png" alt="profile image" id="profile">
     <nav>
         <ul>
-            <li><a href="${relLocation}/index.html">Home</a></li>
-            <li><a href="${relLocation}/pages/about.html">About</a></li>
+            <li><a href="${relLocation}/../index.html">Home</a></li>
+            <li><a href="${relLocation}/about.html">About</a></li>
             <div class="dropdown">
                 <a class="dropbtn">Homework
                     <i class="fa fa-angle-down"></i>
                 </a>
                 <div class="dropdown-content">
-                    <li><a href="${relLocation}/pages/pie.html">Recipe</a></li>
-                    <li><a href="${relLocation}/pages/tmnt.html">Movie</a></li>
-                    <li><a href="${relLocation}/pages/grade.html">Grade</a></li>
-                    <li><a href="${relLocation}/pages/signup.html">Sign up</a></li>
+                    <li><a href="${relLocation}/pie.html">Recipe</a></li>
+                    <li><a href="${relLocation}/tmnt.html">Movie</a></li>
+                    <li><a href="${relLocation}/grade.html">Grade</a></li>
+                    <li><a href="${relLocation}/signup.html">Sign up</a></li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
-                    <li><a href="${relLocation}/pages/tip.html">Tip Calculator</a></li>
+                    <li><a href="${relLocation}/tip.html">Tip Calculator</a></li>
                     <li><a href="${relLocation}/buggy/buggyjs.html">Buggy Js</a></li>
-                    <li><a href="${relLocation}/pages/decoratemytext.html">Decorate Text</a></li>
+                    <li><a href="${relLocation}/decoratemytext.html">Decorate Text</a></li>
                 </div>
             </div>
         </ul>
